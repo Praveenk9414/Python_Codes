603e42029436280454b378f07f64259287e15eb2
Praveenk9414 <praveenk9414@gmail.com>
2024-03-08 19:40:22 +0530

    Day-1- Using print function and dealing with it's error

---
+++ Python_Codes/day1_printfun.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+
+# print("Hello World!)---- This will give you an EOL error while running the code 
+
+print("""This is a string 
+that spans 
+multiple lines""")   # Here not using triple quotes will also give you an EOL error
